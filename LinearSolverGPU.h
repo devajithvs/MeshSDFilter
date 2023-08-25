9ae39e70bec2d1a6f08bbd847b0fc6e89a30f27f
Devajith Valaparambil Sreeramaswamy <devajithvs@gmail.com>
2023-08-25 11:30:23 +0100

    Working CD solver

--- LinearSolverGPU.h
+++ LinearSolverGPU.h
@@ -1,8 +1,9 @@
 #include "SDFilter.h"
 
 #include "cuda_runtime.h"
+#include <cublas_v2.h>
 #include <cusolverSp.h>
-// #include <cusparse.h>
+#include <cusparse.h>
 #include <iostream>
 
 #define CUDA_CHECK(call)                                                       \
@@ -17,9 +18,108 @@
 
 // Linear solver for symmetric positive definite matrix,
 namespace SDFilter {
+void solveUsingConjugateGradient(int N, int nz, int *d_csrRowPtr,
+                                 int *d_csrColInd, double *d_csrVal,
+                                 double *d_b, double *d_x,
+                                 const double tol = 1e-5f,
+                                 const int max_iter = 10000) {
 
-void solveUsingCG(const int n, double *d_A, double *d_p, double *d_r,
-                  double *d_temp, double *d_b, double *d_x) {}
+  cublasHandle_t cublasHandle;
+  cusparseHandle_t cusparseHandle;
+
+  cublasCreate(&cublasHandle);
+  cublasStatus_t blasStatus;
+  cusparseCreate(&cusparseHandle);
+
+  double a, b, na, r1, dot;
+  double alpha = 1.0;
+  double alpham1 = -1.0;
+  double beta = 0.0;
+
+  double r0 = 0.0;
+
+  double *d_p, *d_Ax;
+
+  cudaMalloc((void **)&d_p, N * sizeof(double));
+  cudaMalloc((void **)&d_Ax, N * sizeof(double));
+
+  /* Wrap raw data into cuSPARSE generic API objects */
+  cusparseSpMatDescr_t matA = NULL;
+  cusparseCreateCsr(&matA, N, N, nz, d_csrRowPtr, d_csrColInd, d_csrVal,
+                    CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
+                    CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F);
+  cusparseDnVecDescr_t vecx = NULL;
+  cusparseCreateDnVec(&vecx, N, d_x, CUDA_R_64F);
+  cusparseDnVecDescr_t vecp = NULL;
+  cusparseCreateDnVec(&vecp, N, d_p, CUDA_R_64F);
+  cusparseDnVecDescr_t vecAx = NULL;
+  cusparseCreateDnVec(&vecAx, N, d_Ax, CUDA_R_64F);
+
+  /* Allocate workspace for cuSPARSE */
+  size_t bufferSize = 0;
+  cusparseSpMV_bufferSize(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
+                          &alpha, matA, vecx, &beta, vecAx, CUDA_R_64F,
+                          CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize);
+  void *buffer = NULL;
+  cudaMalloc(&buffer, bufferSize);
+
+  // ... (similar initialization steps as in the main code)
+
+  // Start CG algorithm
+  cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA,
+               vecx, &beta, vecAx, CUDA_R_64F, CUSPARSE_SPMV_ALG_DEFAULT,
+               buffer);
+
+  cublasDaxpy(cublasHandle, N, &alpham1, d_Ax, 1, d_b, 1);
+  blasStatus = cublasDdot(cublasHandle, N, d_b, 1, d_b, 1, &r1);
+
+  int k = 1;
+
+  while (r1 > tol * tol && k <= max_iter) {
+    if (k > 1) {
+      b = r1 / r0;
+      blasStatus = cublasDscal(cublasHandle, N, &b, d_p, 1);
+      blasStatus = cublasDaxpy(cublasHandle, N, &alpha, d_b, 1, d_p, 1);
+    } else {
+      blasStatus = cublasDcopy(cublasHandle, N, d_b, 1, d_p, 1);
+    }
+
+    cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA,
+                 vecp, &beta, vecAx, CUDA_R_32F, CUSPARSE_SPMV_ALG_DEFAULT,
+                 buffer);
+    blasStatus = cublasDdot(cublasHandle, N, d_p, 1, d_Ax, 1, &dot);
+    a = r1 / dot;
+
+    blasStatus = cublasDaxpy(cublasHandle, N, &a, d_p, 1, d_x, 1);
+    na = -a;
+    blasStatus = cublasDaxpy(cublasHandle, N, &na, d_Ax, 1, d_b, 1);
+
+    r0 = r1;
+    blasStatus = cublasDdot(cublasHandle, N, d_b, 1, d_b, 1, &r1);
+    cudaDeviceSynchronize();
+    printf("iteration = %3d, residual = %e\n", k, sqrt(r1));
+    k++;
+  }
+
+  if (matA) {
+    cusparseDestroySpMat(matA);
+  }
+  if (vecx) {
+    cusparseDestroyDnVec(vecx);
+  }
+  if (vecAx) {
+    cusparseDestroyDnVec(vecAx);
+  }
+  if (vecp) {
+    cusparseDestroyDnVec(vecp);
+  }
+
+  // Clean up
+  cusparseDestroy(cusparseHandle);
+  cublasDestroy(cublasHandle);
+  cudaFree(d_p);
+  cudaFree(d_Ax);
+}
 
 void solveUsingCusolver(const int n, const int nnz, const int *d_csrRowPtr,
                         const int *d_csrColInd, const double *d_csrVal,
@@ -83,6 +183,23 @@ public:
       return true;
     } else if (solver_type_ == Parameters::CG) {
 
+      n = M.rows();
+      nnz = M.nonZeros();
+
+      CUDA_CHECK(cudaMalloc((void **)&d_csrVal, nnz * sizeof(double)));
+      CUDA_CHECK(cudaMalloc((void **)&d_csrRowPtr, (n + 1) * sizeof(int)));
+      CUDA_CHECK(cudaMalloc((void **)&d_csrColInd, nnz * sizeof(int)));
+      CUDA_CHECK(cudaMalloc((void **)&d_b, n * sizeof(double)));
+      CUDA_CHECK(cudaMalloc((void **)&d_x, n * sizeof(double)));
+
+      CUDA_CHECK(cudaMemcpy(d_csrVal, M.valuePtr(), nnz * sizeof(double),
+                            cudaMemcpyHostToDevice));
+      CUDA_CHECK(cudaMemcpy(d_csrRowPtr, M.outerIndexPtr(),
+                            (n + 1) * sizeof(int), cudaMemcpyHostToDevice));
+      CUDA_CHECK(cudaMemcpy(d_csrColInd, M.innerIndexPtr(), nnz * sizeof(int),
+                            cudaMemcpyHostToDevice));
+
+      return true;
     } else {
       return false;
     }
@@ -101,6 +218,24 @@ public:
 
         solveUsingCusolver(n, nnz, d_csrRowPtr, d_csrColInd, d_csrVal, d_b,
                            d_x);
+
+        CUDA_CHECK(cudaMemcpy(sol.col(i).data(), d_x, n * sizeof(double),
+                              cudaMemcpyDeviceToHost));
+
+        return true;
+      }
+    } else if (solver_type_ == Parameters::CG) {
+
+      int n_cols = rhs.cols();
+
+      for (int i = 0; i < n_cols; ++i) {
+        const double *b_data = rhs.col(i).data();
+
+        CUDA_CHECK(cudaMemcpy(d_b, b_data, n * sizeof(double),
+                              cudaMemcpyHostToDevice));
+
+        solveUsingConjugateGradient(n, nnz, d_csrRowPtr, d_csrColInd, d_csrVal,
+                                    d_b, d_x);
 
         CUDA_CHECK(cudaMemcpy(sol.col(i).data(), d_x, n * sizeof(double),
                               cudaMemcpyDeviceToHost));
@@ -132,7 +267,6 @@ private:
   Parameters::LinearSolverType solver_type_;
   int n, nnz;
   double *d_csrVal, *d_b, *d_x;
-  double *d_A, d_p, *d_r, *d_temp;
   int *d_csrRowPtr, *d_csrColInd;
 };
 
